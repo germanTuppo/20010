63f05df955c223dae4cf4820152f9251bca1248c
german <germantuppo@gmail.com>
2021-12-07 01:26:40 -0300

    multiplicidadDivisor creo que funcionando

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 let opcion=-1;
-let opciones= "Selecciona lo que quieres hacer: \n1) Ingresar un número para ver si es primo. \n2) Ingresar un número y ver una lista de todos los numeros primos hasta dicho número. \n0) Salir.\n";
- //console.log(primosHasta(100));
+let opciones= "Selecciona lo que quieres hacer: \n1) Ingresar un número para ver si es primo. \n2) Ingresar un número y ver una lista de todos los numeros primos hasta dicho número. \n3) Ingresar un mes en formato numérico para verlo en formato texto y conocer la candidad de días posee. \n4)Ver los divisores de un número \n0) Salir.\n";
+
+/* Nota a Uciel: Las boludeces de la consola con los colores y eso las saqué de acá: https://blog.irontec.com/8-trucos-de-console/*/
+
 while(opcion!=0){
     let numero;
+    let numeroB;
 
     console.log(opciones);//Solo para que se muestren las opciones en la consola
     opcion=parseInt(prompt(opciones));
     switch (opcion){
+        case 0: console.log("%cGracias por confiar en nosotros! Te vamos a extrañar\n\n\n","color:#000;font-size: 14px;background:#0f0;font-weight: bold;")
         case 1: {
             numero=parseInt(prompt("Ingrese el número para saber si es primo"));
-            if (esPrimo(numero)) console.log(`El ${numero} es primo\n`); else console.log(`El ${numero} no es primo\n\n`);
+            if (esPrimo(numero)) console.log(`%cEl ${numero} es primo\n`,"color:#000;font-size: 16px;background:#0f0;font-weight: bold;"); else console.log(`%cEl ${numero} no es primo\n\n`,"color:#000;font-size: 14px;background:#0f0;font-weight: bold;");
             break;           
         }
         case 2: {
             numero=parseInt(prompt("Ingrese el número para conocer todos los números primos hasta ese número, no te zarpes pls!"));
-            console.log(`Los numeros primos hasta el ${numero} son:\n${primosHasta(numero)}\n\n`);
+            console.log(`%cLos numeros primos hasta el ${numero} son:\n${primosHasta(numero)}\n\n`,"color:#000;font-size: 14px;background:#0f0;font-weight: bold;");
+            break;
+        }
+        case 3: {
+            numero=parseInt(prompt("Ingrese un mes (en formato numérico) para conocer su nombre y la cantidad de dias que tiene."));
+            console.log(`%cEl mes de ingresado es: ${mesTexto(numero)} y tiene ${mesDias(numero)} dias\n\n`,"color:#000;font-size: 14px;background:#0f0;font-weight: bold;");
+            break;
+        }
+        case 4: {
+            numero=parseInt(prompt("Ingrese dividendo:"));
+            numeroB=parseInt(prompt("Ingrese el divisor:"));
+            console.log (`${multiplicidadDivisor(numero, numeroB)}\n`)
             break;
         }
 
@@ -24,6 +39,15 @@ while(opcion!=0){
 
 }
 
+
+
+
+
+/***************************************************************************************************************
+ * 
+ *FUNCIONES
+ * 
+ ***************************************************************************************************************/
 
 function esPrimo(numero){
     /*Regresa 1 si el número ingregado es primo (Es decir tiene exactamente dos divisores),
@@ -60,10 +84,77 @@ function primosHasta (numero){
         return cadena;
     }    
 }
+/*Ya que estamos con el algebra jajaj, me gustaría hacer:
+    --> Algo que calcule todos los divisores de un número
+    --> Alguna funcion que calcule el mcd y otra para el mcm
+    --> cuando veamos arrays trabajar con matrices (suma resta multiplicacion division determinantes resolucion de un sistema de m ecuaciones con m incognitas.
+        
+        Me pongo con ello!!!*/
+
+function multiplicidadDivisor(num, divisor){
+    /*EN PROCESO! Debería mostrar cuantas veces es un número divisor de otro */
+    let aux=0;
+    let cociente;
+
+    if(num%1!==0 || divisor==1){//por las dudas que el que el usuario ponga 1 como divisor y se pudra todo (me pasó)
+        return -1;
+    } else
+        if (num%divisor!=0){
+            return 0;
+        }
+            else {
+                cociente=num;
+                while (cociente%divisor==0){
+                    aux++;
+                    cociente=cociente/divisor;
+                    console.log("toy en el while");
+                }
+                return aux;
+            }
+}
+
+function primosDivisores(numero){
+    /*EN PROCESO! Debería mostrar todos los divisores de un número */
+}
+
+function mcm (numA, numB){
+    /*EN PROCESO! calcular el minimo común multiplo de los dos numeros */
+}
+
+function mcd (numA, numB){
+    /*EN PROCESO! calcular el maximo común divisor de los dos numeros */
+}
+
+function sumaMatrices(matrizA, matrizB) {
+    /*EN PROCESO! calcular la suma de dos matrices*/
+}
+
+function restaMatrices(matrizA, matrizB) {
+    /*EN PROCESO! calcular la resta de dos matrices*/
+}
+
+function productoMatrices(matrizA, matrizB) {
+    /*EN PROCESO! calcular el producto de dos matrices*/
+}
+
+function inversaMatriz(matrizA) {
+    /*EN PROCESO! calcular la inversa de una matriz*/
+}
+
+function determinante(matrizA) {
+    /*EN PROCESO! calcular el determinante de una matriz*/
+}
+
+function resolverSistema(matrizA) {
+    /*EN PROCESO! resolver un sistema de ecuaciones con n incognitas*/
+}
 
 
+
+
+/*de aca había arrancado para desafios anteriores, pero lo "refactorice" para que tenga mas sentido a lo que ya estuvimos viendo!*/
 function mesDias(mes){
-    
+    /*Función que indica dado un mes la cantidad de dias que posee*/
     switch(parseInt(mes)){
         case 1:
             return 31;
@@ -121,4 +212,3 @@ function mesTexto(mes){
             return "Diciembre";           
     }
 }
-/*********************************************************************/
